93769be67a64213a36615de4c3a02d538bebc090
maria badasian <badam190@newschool.edu>
2016-03-17 10:56:42 -0400

    quiz

---
+++ quiz3/assets/js/app.js
@@ -0,0 +1,10 @@
+var color = "yellow";
+var wallet = "1,100";
+var converted_wallet = "0";
+var conversion_rate = ["0.88", "111.67", ".69"];
+
+var convert_currency(amount, converter) {
+	//figure out how to write out the code necessary to call the function convert_currency
+	// passing in your wallet variable as the amount and yen as the conversion
+	// are .. code in html 
+}
